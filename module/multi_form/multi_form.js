c97eea6009eb3d671c54e1f9e87ff4fe10df48ad
Rich Frausto <vindigo@gmail.com>
2018-03-13 23:24:23 -0700

    20180313: fix line 48

--- module/multi_form/multi_form.js
+++ module/multi_form/multi_form.js
@@ -17,7 +17,8 @@ var multiForm = (function(config){
     count = steps.length,
     submitBtnID = "#" + config.submitBtnName,
     submitBtnEl = document.querySelector(submitBtnID),
-    wrapper, commands, legend, legendText;
+    wrapper, commands, legend, legendText,stepsUL;
+
 
     hide(submitBtnEl);
 
@@ -36,9 +37,26 @@ var multiForm = (function(config){
 
       legend = el.querySelector("legend");
       legendText = legend.innerHTML;
-      // legend.appendChild("<li id='stepDesc" + i + "'>Step " + (i + 1) + "<span>" + name + "</span></li>");
-console.log(el);
-// $("#steps").append("<li id='stepDesc" + i + "'>Step " + (i + 1) + "<span>" + name + "</span></li>");
+
+      stepsUL = document.querySelector("#steps");
+      stepsUL.insertAdjacentHTML("beforeend", "<li id='stepDesc" + i + "'>Step " + (i + 1) + "<span>" + legendText + "</span></li>");
+
+      if (i == 0) {
+        createNextButton(i);
+        selectStep(i);
+      } else if (i == count - 1) {
+        var stepEl = document.querySelector("#step" + i);
+        hide(stepEl);
+
+        // $("#step" + i).hide();
+        // createPrevButton(i);
+      } else {
+        var stepEl = document.querySelector("#step" + i);
+        hide(stepEl);
+
+        createPrevButton(i);
+        createNextButton(i);
+      }
 
 
 
@@ -46,8 +64,92 @@ console.log(el);
     });
 
     // functions
+
+
+    function createNextButton(i) {
+      var stepName = "step" + i;
+      var stepNameNext = "step" + (i + 1);
+      document.querySelector("#" + stepName + "commands").insertAdjacentHTML("beforeend","<a href='#' id='" + stepName + "Next' class='next'>Next ></a>");
+
+      document.querySelector("#" + stepName + "Next").addEventListener("click", function(e){
+        var stepNameEl = document.querySelector("#" + stepName),
+            stepNameNextEl = document.querySelector("#" + stepNameNext);
+
+        hide( stepNameEl );
+        show( stepNameNextEl );
+
+        if (i + 2 == count){
+          console.log("add submit button")
+        // $(submmitButtonName).show();
+        }
+
+        selectStep(i + 1);
+      })
+    }
+
+    function selectStep(i) {
+      var stepsEl = document.querySelector("#steps li"),
+          stepDescEl = document.querySelector("#stepDesc" + i),
+          stepDescPrevEl = document.querySelector("#stepDesc" + (i - 1)),
+          stepDescNextEl = document.querySelector("#stepDesc" + (i + 1));
+
+      if (stepDescPrevEl){ removeClass(stepDescPrevEl, "current") }
+      if (stepDescNextEl){ removeClass(stepDescNextEl, "current") }
+
+      addClass(stepDescEl, "current");
+
+    }
+
+    function createPrevButton(i) {
+      var stepName = "step" + i;
+      var stepNameCommandsEl = document.querySelector("#" + stepName + "commands");
+
+      stepNameCommandsEl.insertAdjacentHTML("beforeend", "<a href='#' id='" + stepName + "Prev' class='prev'>< Back</a>");
+
+      var stepNamePrevEl = document.querySelector("#" + stepName + "Prev");
+
+      stepNamePrevEl.addEventListener("click", function(){
+          var stepNameEl = document.querySelector("#" + stepName),
+              stepNamePrevEl = document.querySelector("#step" + (i - 1));
+
+          hide(stepNameEl);
+          show(stepNamePrevEl);
+         selectStep(i - 1);
+      })
+
+      // $("#" + stepName + "Prev").bind("click", function(e) {
+      //   $("#" + stepName).hide();
+      //   $("#step" + (i - 1)).show();
+      //   $(submmitButtonName).hide();
+      //   selectStep(i - 1);
+      // });
+    }
+
+
+    function addClass(el, className){
+      if (el.classList){
+        el.classList.add(className);
+      } else {
+        el.className += ' ' + className;
+      }
+    }
+
+    function removeClass(el, className) {
+console.log("classList: " + el.classList);
+      if (el.classList){
+        el.classList.remove(className);
+      } else {
+        el.className = el.className.replace(new RegExp('(^|\\b)' + className.split(' ').join('|') + '(\\b|$)', 'gi'), ' ');
+      }
+
+    }
+
     function hide(el){
-      el.setAttribute("hidden", true);
+      el.style.display = "none";
+    }
+
+    function show(el){
+      el.style.display = "block";
     }
 
     function wrap(toWrap, wrapper) {
