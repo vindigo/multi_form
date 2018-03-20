ba2e2cb63cca344b123b51cfb03841a3c57fc37a
Rich Frausto <vindigo@gmail.com>
2018-03-20 10:28:40 -0700

    good version

--- module/multi_form/multi_form.js
+++ module/multi_form/multi_form.js
@@ -6,7 +6,9 @@
 */
 var config = {
   formName:"SignupForm",
-  submitBtnName: "SaveAccount"
+  submitBtnName: "SaveAccount",
+  cancelAction: "",
+  submitAction: ""
 }
 
 var multiForm = (function(config){
@@ -17,6 +19,8 @@ var multiForm = (function(config){
     count = steps.length,
     submitBtnName = config.submitBtnName,
     submitBtnEl = document.querySelector("#" + submitBtnName),
+    cancelBtnEl = document.querySelector("#Cancel"),
+    toolbarEl = document.querySelector("#toolbar"),
     wrapper, commands, legend, legendText,stepsUL;
 
     formNameEl = document.querySelector("#" + formName)
@@ -24,20 +28,17 @@ var multiForm = (function(config){
     // insert 'steps' ul at top of container
     container.insertAdjacentHTML('afterbegin', "<ul id='steps'></ul>");
 
-
     Array.prototype.forEach.call(steps, function(el, i){
 
       // wrap each fieldset with 'steps' div
       wrapper = document.createElement('div');
       wrapper.id = "step" + i;
-      el.parentNode.appendChild(wrapper);
-      wrapper.appendChild(el)
-      //wrap(el, wrapper);
+      wrap(el, wrapper);
 
       // append 'commands' paragraph to the bottom of each fieldset
       commands = document.createElement("p");
       commands.id = "step" + i + "commands";
-      el.appendChild(commands);
+      el.append(commands);
 
       // get legend label
       legend = el.querySelector("legend");
@@ -57,25 +58,27 @@ var multiForm = (function(config){
         var stepEl = document.querySelector("#step" + i);
         hide(stepEl);
         createPrevButton(i);
+        // console.log("lastStep " + lastStep);
+        // container.insertAdjacentHTML('afterbegin', "<ul id='steps'></ul>");
+
       } else {
         var stepEl = document.querySelector("#step" + i);
         hide(stepEl);
         createPrevButton(i);
         createNextButton(i);
       }
-
     });
 
-    // inset submit button at end of formNam
-    // formNameEl.insertAdjacentHTML("beforeend", '<input id="'+ submitBtnName +'" class="right" type="button" value="Submit form" />');
+    // inset toolbar at end of form
+    formNameEl.append(toolbarEl);
 
 
-    // functions
+    // functions ==============================
     function createNextButton(i) {
       var stepName = "step" + i;
       var stepNameNext = "step" + (i + 1);
 
-      document.querySelector("#" + stepName + "commands").insertAdjacentHTML("beforeend","<a href='#' id='" + stepName + "Next' class='next'>Next ></a>");
+      document.querySelector("#" + stepName + "commands").insertAdjacentHTML("beforeend","<a href='#' id='" + stepName + "Next' class='btn btn-primary next'>Next ></a>");
 
       document.querySelector("#" + stepName + "Next").addEventListener("click", function(e){
         var stepNameEl = document.querySelector("#" + stepName),
@@ -85,7 +88,7 @@ var multiForm = (function(config){
         show( stepNameNextEl );
 
         if (i + 2 == count){
-          console.log(submitBtnEl)
+          show(cancelBtnEl);
           show(submitBtnEl);
         }
 
@@ -103,14 +106,13 @@ var multiForm = (function(config){
       if (stepDescNextEl){ removeClass(stepDescNextEl, "current") }
 
       addClass(stepDescEl, "current");
-
     }
 
     function createPrevButton(i) {
       var stepName = "step" + i;
       var stepNameCommandsEl = document.querySelector("#" + stepName + "commands");
 
-      stepNameCommandsEl.insertAdjacentHTML("beforeend", "<a href='#' id='" + stepName + "Prev' class='prev'>< Back</a>");
+      stepNameCommandsEl.insertAdjacentHTML("beforeend", "<a href='#' id='" + stepName + "Prev' class='btn btn-primary prev'>< Back</a>");
 
       var stepNamePrevEl = document.querySelector("#" + stepName + "Prev");
 
@@ -120,15 +122,14 @@ var multiForm = (function(config){
 
           hide(stepNameEl);
           show(stepNamePrevEl);
-          if (submitBtnEl){
-              hide(submitBtnEl); // TODO: fix this
-          }
+          if (submitBtnEl){ hide(submitBtnEl); }
+          if (cancelBtnEl){ hide(cancelBtnEl); }
 
           selectStep(i - 1);
       })
     }
 
-    // helper functions
+    // helpers ==============================
     function addClass(el, className){
       if (el.classList){
         el.classList.add(className);
@@ -146,96 +147,19 @@ var multiForm = (function(config){
     }
 
     function hide(el){
-      el.style.display = "none";
+      el.classList.add("d-none");
+      // el.style.display = "none";
     }
 
     function show(el){
-      el.style.display = "block";
+      // el.style.display = "block";
+      el.classList.remove("d-none");
     }
 
     function wrap(toWrap, wrapper) {
       wrapper = wrapper || document.createElement('div');
-      toWrap.parentNode.appendChild(wrapper);
-      return wrapper.appendChild(toWrap);
+      toWrap.parentNode.append(wrapper);
+      return wrapper.append(toWrap);
     }
 
-    // el.parentNode.appendChild(wrapper);
-
 })(config);
-
-
-// $(document).ready(function() {
-//   $("#SignupForm").formToWizard({
-//     submitButton: 'SaveAccount'
-//   })
-// });
-//
-//
-// (function($) {
-//   $.fn.formToWizard = function(options) {
-//     options = $.extend({
-//       submitButton: ""
-//     }, options);
-//
-//     var element = this;
-//
-//     var steps = $(element).find("fieldset");
-//     var count = steps.length;
-//
-//     var submmitButtonName = "#" + options.submitButton;
-//     $(submmitButtonName).hide();
-//
-//     $(element).before("<ul id='steps'></ul>");
-//
-//     steps.each(function(i) {
-//       $(this).wrap("<div id='step" + i + "'></div>");
-//       $(this).append("<p id='step" + i + "commands'></p>");
-//
-//       var name = $(this).find("legend").html();
-//       $("#steps").append("<li id='stepDesc" + i + "'>Step " + (i + 1) + "<span>" + name + "</span></li>");
-//
-//       if (i == 0) {
-//         createNextButton(i);
-//         selectStep(i);
-//       } else if (i == count - 1) {
-//         $("#step" + i).hide();
-//         createPrevButton(i);
-//       } else {
-//         $("#step" + i).hide();
-//         createPrevButton(i);
-//         createNextButton(i);
-//       }
-//     });
-//
-//     function createPrevButton(i) {
-//       var stepName = "step" + i;
-//       $("#" + stepName + "commands").append("<a href='#' id='" + stepName + "Prev' class='prev'>< Back</a>");
-//
-//       $("#" + stepName + "Prev").bind("click", function(e) {
-//         $("#" + stepName).hide();
-//         $("#step" + (i - 1)).show();
-//         $(submmitButtonName).hide();
-//         selectStep(i - 1);
-//       });
-//     }
-//
-//     function createNextButton(i) {
-//       var stepName = "step" + i;
-//       $("#" + stepName + "commands").append("<a href='#' id='" + stepName + "Next' class='next'>Next ></a>");
-//
-//       $("#" + stepName + "Next").bind("click", function(e) {
-//         $("#" + stepName).hide();
-//         $("#step" + (i + 1)).show();
-//         if (i + 2 == count)
-//           $(submmitButtonName).show();
-//         selectStep(i + 1);
-//       });
-//     }
-//
-//     function selectStep(i) {
-//       $("#steps li").removeClass("current");
-//       $("#stepDesc" + i).addClass("current");
-//     }
-//
-//   }
-// })(jQuery);
